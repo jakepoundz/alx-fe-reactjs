96c388077f7e6e7e3ea4b1c8c32e48900e9cc6c2
jakepoundz <fiifijake888@gmail.com>
2024-09-07 08:50:42 +0000

    updated some files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
+/** @type {import('tailwindcss').Config}  */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", 
+    "./public/index.html"],
   darkMode: false,
-  theme: { extend: {} },
+  theme: {
+     extend: {} },
   variants: { extend: {} },
   plugins: [],
 };
