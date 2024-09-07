3895e3b3871cf881e1bd2bda8f76ebd434c7741b
jakepoundz <fiifijake888@gmail.com>
2024-09-07 21:59:11 +0000

    i updated my user profile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,15 +4,18 @@ function UserProfile() {
     <div
       className="user-profile bg-gray-100 rounded-lg shadow-lg mx-auto my-20 
               p-4 sm:p-4 md:p-8 
-              max-w-xs sm:max-w-sm md:max-w-sm"
+              max-w-xs sm:max-w-sm md:max-w-sm
+              hover:shadow-xl transition-shadow duration-300 ease-in-out"
     >
       <img
         src="https://via.placeholder.com/150"
         alt="User"
         className="rounded-full mx-auto 
-                  w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36"
+                  w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36
+                  hover:scale-110 transition-transform duration-300 ease-in-out "
       />
-      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4">
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4
+      hover:text-blue-500 transition-colors duration-300 ease-in-out">
         John Doe
       </h1>
       <p className="text-sm sm:text-base md:text-base text-gray-600">
