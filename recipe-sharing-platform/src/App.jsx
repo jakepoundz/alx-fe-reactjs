89aba018d0a530f6572eafb719989f1d8fb1f3c6
jakepoundz <fiifijake888@gmail.com>
2024-09-10 13:51:32 +0000

    i created a recipe sharing platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="text-blue-500">
+      <h1>Hello, Tailwind!</h1>
+    </div>
+  );
+}
+export default App;
