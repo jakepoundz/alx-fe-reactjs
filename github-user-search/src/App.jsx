3257373bebcca04fb66fa48ee49ea4defe764d98
jakepoundz <fiifijake888@gmail.com>
2024-09-17 15:03:08 +0000

    created a new user search app

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      {/* Your application content will go here */}
+    </div>
+  );
+}
+export default App;
